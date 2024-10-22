56ae775b271db113556ddf0bc6bcc90ee3319e01
JhonmaSG <jmserranog@udistrital.edu.co>
2024-10-22 14:56:21 -0500

    Fix Names and strucuture proyect

--- app/src/main/java/com/example/guarderiaciudadbolivar/InscripcionChildrenActivity.kt
+++ app/src/main/java/com/example/guarderiaciudadbolivar/InscripcionChildrenActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class AuthorizedPersonsActivity : AppCompatActivity() {
+class InscripcionChildrenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_authorized_persons)
+        setContentView(R.layout.activity_inscripcion_children)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
